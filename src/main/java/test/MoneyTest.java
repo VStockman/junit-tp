68e448bd595cec2caeab0e0e58f462820b7ac790
Valentin Stockman <valentin@smartnsoft.com>
2020-12-04 15:09:06 +0100

    First commit

---
+++ src/main/java/test/MoneyTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import src.Money;
+
+
+public class MoneyTest {
+	
+	private Money money = new Money(10, "EUR");
+
+	@org.junit.Test
+	public void CheckPositiveAmountTest() {
+		assertTrue(money.Amount() >= 0);
+	}
+	
+	@org.junit.Test
+	public void ISORuleForCurrencyTest() {
+		assertTrue(money.Currency().length() == 3);
+		assertTrue(money.Currency() == "EUR" 
+				|| money.Currency() == "USD" 
+				|| money.Currency() == "CHF" 
+				|| money.Currency() == "GBP");
+	}
+	
+	@org.junit.Test
+	public void AdditionTest() {
+		Money moneyToAddEur = new Money(20, "EUR");
+		Money moneyToAddUsd = new Money(40, "USD");
+		
+		assertEquals(money.add(moneyToAddEur).Amount(), 30);
+	}
+}
